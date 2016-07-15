1e2cdb3aaa5fc9f1c8ce1348178780d1464804bb
Anmol Mago <anmolmago@gmail.com>
2016-07-14 20:36:14 -0400

    create raw unaltered datasets, add reference papers, other stuff

---
+++ data/duplicates.py
@@ -0,0 +1,8 @@
+with open('negcorpus.csv') as f:
+    seen = set()
+    for line in f:
+        line_lower = line.lower()
+        if line_lower in seen:
+            print(line)
+        else:
+            seen.add(line_lower)
